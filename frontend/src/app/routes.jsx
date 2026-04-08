611401c63fff254226699c33a1d83b3638864670
Harshit Vashisht <iamHV856156@gmail.com>
2026-04-08 22:57:31 +0530

    updated routes

--- frontend/src/app/routes.jsx
+++ frontend/src/app/routes.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import login from "../features/auth/pages/login";
+import Login from "../features/auth/pages/login";
 import Signup from "../features/auth/pages/signup";
+import Dashboard from "@/pages/dashBoard";
+import ProtectedRoute from "@/features/auth/components/protectedRoute";
 export default function AppRoutes(){
     return(
         <BrowserRouter>
         <Routes>
-            <Route path="/login" element={<login/>}/>
+            <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<Signup/>}/>
+            <Route path="/dashboard" element={
+            <ProtectedRoute>
+                <Dashboard/>
+            </ProtectedRoute>}/>
         </Routes>
         </BrowserRouter>
     );
