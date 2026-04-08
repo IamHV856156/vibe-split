2dd001a75ff7651a941fdf7f22b0cca607fe80f8
Harshit Vashisht <iamHV856156@gmail.com>
2026-04-08 22:58:38 +0530

    updated main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
-import ReactDom from 'react-dom/client'
-import AppRoutes from './app/routes'
-import { supabase } from './services/supabaseClient'
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import AppRoutes from './app/routes';
 import { AuthProvider } from './context/authContext';
 
-supabase.auth.getSession().then(({data})=>{
-  console.log("Session:",data.session);
-});
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
     <AppRoutes />
     </AuthProvider>
   </React.StrictMode>,
-)
+);
