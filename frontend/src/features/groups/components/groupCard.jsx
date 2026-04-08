ba1b0411a72d1acc38264dbbd8f8c7caaf7a91ef
Harshit Vashisht <iamHV856156@gmail.com>
2026-04-08 23:35:58 +0530

    created groupcard structure

---
+++ frontend/src/features/groups/components/groupCard.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { createGroup } from "../groupServices";
+import { useAuth } from "@/context/authContext";
+
+export default function CreateGroup(){
+    const[name,setName]=useState("");
+    const {user} = useAuth();
+
+    const handleCreate = async ()=>{
+      const{error} = await createGroup(name,user.id);
+      if(error){
+        alert(error.message);
+      }else{
+        alert("Group created");
+        setName("");
+      }
+  }  
+};
+
+return(
+    <div>
+        <input placeholder="Group Name" value={name} onChange={(e)=>setName(e.target.value)}/>
+        <button onClick={handleCreate}>Create Group</button>
+    </div>
+);
