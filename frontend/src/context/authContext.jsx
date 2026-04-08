74a9bbca8fd0a4fadf96f5c5b6d276f274c685c0
Harshit Vashisht <iamHV856156@gmail.com>
2026-04-08 22:41:50 +0530

    created authContext structure

---
+++ frontend/src/context/authContext.jsx
@@ -0,0 +1,25 @@
+import {createContext,useContext,useEffect, useState} from "react";
+import { supabase } from "@/services/supabaseClient";
+
+const authContext = createContext();
+
+export const AuthProvider = ({children}) =>{
+    const [user,setUser] = useState(null);
+
+    useEffect(()=>{
+        supabase.auth.getUser().then(({data})=>setUser(data.user));
+
+        const {data:listner} = supabase.auth.onAuthStateChange((_, session)=>{
+            setUser(session?.user || null);
+        });
+
+        return () => listner.subscription.unsubscribe();
+    },[]);
+    return(
+        <authContext.Provider value={{user}}>
+            {children}
+        </authContext.Provider>
+    );
+};
+
+export const useAuth =() => useContext(authContext);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import AppRoutes from './app/routes'
+import { supabase } from './services/supabaseClient'
+import { AuthProvider } from './context/authContext';
+
+supabase.auth.getSession().then(({data})=>{
+  console.log("Session:",data.session);
+});
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <AppRoutes />
+    </AuthProvider>
   </React.StrictMode>,
 )
