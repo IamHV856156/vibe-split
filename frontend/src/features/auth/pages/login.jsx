38a991b562f72e25653af2448dea0339f8c00dcb
Harshit Vashisht <iamHV856156@gmail.com>
2026-04-08 22:45:53 +0530

    added navigate also

--- frontend/src/features/auth/pages/login.jsx
+++ frontend/src/features/auth/pages/login.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { signIn } from "../authServices";
+import { useNavigate } from "react-router-dom";
 
 export default function Login(){
     const[email, setEmail] = useState("");
     const[password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleLogin = async()=>{
         const {error} = await signIn(email,password);
         if(error){
             alert(error.message);
+            navigate("/dashboard");
         };
     };
 
