05cff9740cb228ccb5538268a86c57893c54054e
Harshit Vashisht <iamHV856156@gmail.com>
2026-04-11 04:01:27 +0530

    cleaned dashboard

--- frontend/src/pages/dashBoard.jsx
+++ frontend/src/pages/dashBoard.jsx
@@ -1,36 +1,16 @@
-import { useAuth } from "@/context/authContext";
-import CreateGroup from "@/features/groups/components/createGroup";
-import { logOut } from "@/features/auth/authServices";
 import { useGroups } from "@/features/groups/useGroups";
-import GroupCard from "@/features/groups/components/groupCard";
+import GroupCard from "@/features/groups/components/GroupCard";
 
-export default function Dashboard(){
-    const {user} =useAuth();
-    const {groups,loading, fetchGroups} = useGroups(user?.id);
+export default function Dashboard() {
+  const { groups } = useGroups();
 
-    const handleLogout = async () =>{
-        const {error} = await logOut();
-        if (error) {
-            console.log("Logout Error:",error);
-        }
-    };
-    if(loading){
-        return(<p>Loading groups...</p>);
-    }
+  return (
+    <div>
+      <h2>Dashboard</h2>
 
-    return(
-        <div>
-            <h2>DashBoard</h2>
-            <p>Welcome {user?.email}</p>
-
-            <button onClick={handleLogout}>Logout</button>
-
-            <h3>Create Group</h3>
-            <CreateGroup onGroupCreated={fetchGroups} />
-            <h3>Your Groups:</h3>
-            {groups.map((g)=>(
-                <GroupCard key={g.id} group={g} user={user}/>
-            ))}
-        </div>
-    );
+      {groups.map((g) => (
+        <GroupCard key={g.id} group={g} />
+      ))}
+    </div>
+  );
 }
